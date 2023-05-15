42dc312e570c93c465599090f9876369de04be6f
Santiago Valdez <valdezsantiago16@gmail.com>
2023-05-15 05:39:36 -0300

    Ya se cargaron todas las tablas a sus versiones en clases

---
+++ SpringBoot_BE/src/main/java/com/vsz/SpringBoot_BE/model/Conocimiento.java
@@ -0,0 +1,29 @@
+package com.vsz.SpringBoot_BE.model;
+
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/** @author Santiago Valdez */
+
+@Getter @Setter
+@Entity
+@Table(name = "conocimiento")
+public class Conocimiento {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "titulo", length = 45, nullable = false)
+    private String titulo;
+
+    @Column(name = "descripcion", length = 45, nullable = false)
+    private String descripcion;
+	
+	@ManyToOne(fetch = FetchType.LAZY) // Relación muchos a uno con Persona
+    @JoinColumn(name = "persona_id", referencedColumnName = "id", nullable = false)
+    private Persona persona;
+	
+}
